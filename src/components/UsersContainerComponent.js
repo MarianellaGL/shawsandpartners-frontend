a883d383523497c5debf3e7e106e6c429b63339b
marianellagomezlunaext <marianellagomez@comafi.com.ar>
2023-01-17 01:22:26 -0300

    final styles

--- src/components/UsersContainerComponent.js
+++ src/components/UsersContainerComponent.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Col, Button } from "react-bootstrap";
 import UserComponent from "./UserComponent";
-import DetailsComponent from "./DetailsComponent";
 import useFetch from "../hooks/useFetch";
-import RepositoriesComponent from "./RepositoriesComponent";
 import { Link } from "react-router-dom";
 
 
@@ -11,7 +9,7 @@ const UsersContainerComponent = (props) => {
     let [since, setSince] = useState(0);
     const users = useFetch(`https://shawandpartners-api-backend.onrender.com/api/users?since=${since}`, null);
 
-    const onClickShowMore = () => {
+    const onClickNext = () => {
         setSince(since += 30);
     }
 
@@ -19,7 +17,7 @@ const UsersContainerComponent = (props) => {
         return <>Loading...</>
     }
 
-    const onClickShowLess = () => {
+    const onClickPrev = () => {
         if (since !== 0) {
             setSince(since -= 30)
         }
@@ -29,17 +27,16 @@ const UsersContainerComponent = (props) => {
 
         <div>
             {
-                users.map((user, index) => <div><Link to={`/${user.login}/details`} className="user-link">
+                users.map((user, index) => <Link to={`/${user.login}/details`} className="user-link" key={index}>
                     <UserComponent key={index} login={user.login} id={user.id} />
-                </Link></div>)
+                </Link>)
             }
         </div>
-        <Button onClick={onClickShowMore}>Show more</Button>
         {since > 0 ?
-            <Button onClick={onClickShowLess} variant="secondary">Show Less</Button>
+            <Button onClick={onClickPrev} variant="secondary">← Previous</Button>
             : ""
         }
-
+        <Button onClick={onClickNext}>Next →</Button>
     </Col>
     </>
 
